1edb1c3ae8f12a2d49c914f96691fe357554fedd
Jan Weiß <jan@geheimwerk.de>
2013-11-02 18:53:30 +0100

    Updating ImageAndTextCell.

--- Classes/Views/ImageAndTextCell.m
+++ Classes/Views/ImageAndTextCell.m
@@ -1,250 +1,158 @@
-//
-// File:	   ImageAndTextCell.m
-//
-// Abstract:   Subclass of NSTextFieldCell which can display text and an image simultaneously.
-//
-// Version:	   1.0
-//
-// Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple Inc. ("Apple")
-//			   in consideration of your agreement to the following terms, and your use,
-//			   installation, modification or redistribution of this Apple software
-//			   constitutes acceptance of these terms.  If you do not agree with these
-//			   terms, please do not use, install, modify or redistribute this Apple
-//			   software.
-//
-//			   In consideration of your agreement to abide by the following terms, and
-//			   subject to these terms, Apple grants you a personal, non - exclusive
-//			   license, under Apple's copyrights in this original Apple software ( the
-//			   "Apple Software" ), to use, reproduce, modify and redistribute the Apple
-//			   Software, with or without modifications, in source and / or binary forms;
-//			   provided that if you redistribute the Apple Software in its entirety and
-//			   without modifications, you must retain this notice and the following text
-//			   and disclaimers in all such redistributions of the Apple Software. Neither
-//			   the name, trademarks, service marks or logos of Apple Inc. may be used to
-//			   endorse or promote products derived from the Apple Software without specific
-//			   prior written permission from Apple.	 Except as expressly stated in this
-//			   notice, no other rights or licenses, express or implied, are granted by
-//			   Apple herein, including but not limited to any patent rights that may be
-//			   infringed by your derivative works or by other works in which the Apple
-//			   Software may be incorporated.
-//
-//			   The Apple Software is provided by Apple on an "AS IS" basis.	 APPLE MAKES NO
-//			   WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
-//			   WARRANTIES OF NON - INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-//			   PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION
-//			   ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-//
-//			   IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
-//			   CONSEQUENTIAL DAMAGES ( INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//			   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//			   INTERRUPTION ) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION
-//			   AND / OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER
-//			   UNDER THEORY OF CONTRACT, TORT ( INCLUDING NEGLIGENCE ), STRICT LIABILITY OR
-//			   OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Copyright (C) 2007 Apple Inc. All Rights Reserved.
-//
+/*
+     File: ImageAndTextCell.m
+ Abstract: Subclass of NSTextFieldCell which can display text and an image simultaneously.
+  Version: 1.2
+ 
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2012 Apple Inc. All Rights Reserved.
+ 
+*/
 
 #import "ImageAndTextCell.h"
+#import <AppKit/NSCell.h>
 
 @implementation ImageAndTextCell
 
-#define kIconImageSize		22.0
-
-#define kImageOriginXOffset 3
-#define kImageOriginYOffset 0
-
-#define kTextOriginXOffset	2
-#define kTextOriginYOffset	2
-#define kTextHeightAdjust	4
-
-// -------------------------------------------------------------------------------
-//	init:
-// -------------------------------------------------------------------------------
-- (id)init
-{
-	self = [super init];
-	
-	// we want a smaller font
-	[self setFont:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]]];
-
-	return self;
+- (id)init {
+    if ((self = [super init])) {
+        [self setLineBreakMode:NSLineBreakByTruncatingTail];
+        [self setSelectable:YES];
+    }
+    return self;
 }
 
-// -------------------------------------------------------------------------------
-//	dealloc:
-// -------------------------------------------------------------------------------
-- (void)dealloc
-{
-    [image release];
-    image = nil;
+- (void)dealloc {
+    [_image release];
     [super dealloc];
 }
 
-// -------------------------------------------------------------------------------
-//	copyWithZone:zone
-// -------------------------------------------------------------------------------
-- (id)copyWithZone:(NSZone*)zone
-{
-    ImageAndTextCell *cell = (ImageAndTextCell*)[super copyWithZone:zone];
-    cell->image = [image retain];
+- (id)copyWithZone:(NSZone *)zone {
+    ImageAndTextCell *cell = (ImageAndTextCell *)[super copyWithZone:zone];
+    // The image ivar will be directly copied; we need to retain or copy it.
+    cell->_image = [_image retain];
     return cell;
 }
 
-// -------------------------------------------------------------------------------
-//	setImage:anImage
-// -------------------------------------------------------------------------------
-- (void)setImage:(NSImage*)anImage
-{
-    if (anImage != image)
-	{
-        [image release];
-        image = [anImage retain];
-		//[image setSize:NSMakeSize(kIconImageSize, kIconImageSize)];
+@synthesize image = _image;
+
+- (NSSize)_imageSize {
+    return NSMakeSize(16, 16);
+}
+
+- (NSRect)imageRectForBounds:(NSRect)cellFrame {
+    NSRect result;
+    if (_image != nil) {
+        result.size = [self _imageSize];
+        result.origin = cellFrame.origin;
+        result.origin.x += 3;
+        result.origin.y += ceil((cellFrame.size.height - result.size.height) / 2);
+    } else {
+        result = NSZeroRect;
     }
+    return result;
 }
 
-// -------------------------------------------------------------------------------
-//	image:
-// -------------------------------------------------------------------------------
-- (NSImage*)image
-{
-    return image;
-}
-
-// -------------------------------------------------------------------------------
-//	isGroupCell:
-// -------------------------------------------------------------------------------
-- (BOOL)isGroupCell
-{
-    return ([self image] == nil && [[self title] length] > 0);
-}
-
-// -------------------------------------------------------------------------------
-//	titleRectForBounds:cellRect
-//
-//	Returns the proper bound for the cell's title while being edited
-// -------------------------------------------------------------------------------
-- (NSRect)titleRectForBounds:(NSRect)cellRect
-{	
-	// the cell has an image: draw the normal item cell
-	NSSize imageSize;
-	NSRect imageFrame;
-
-	imageSize = [image size];
-	NSDivideRect(cellRect, &imageFrame, &cellRect, 3 + imageSize.width, NSMinXEdge);
-
-	imageFrame.origin.x += kImageOriginXOffset;
-	imageFrame.origin.y -= kImageOriginYOffset;
-	imageFrame.size = imageSize;
-	
-	imageFrame.origin.y += ceil((cellRect.size.height - imageFrame.size.height) / 2);
-	
-	NSRect newFrame = cellRect;
-	newFrame.origin.x += kTextOriginXOffset;
-	newFrame.origin.y += kTextOriginYOffset;
-	newFrame.size.height -= kTextHeightAdjust;
-
-	return newFrame;
-}
-
-// -------------------------------------------------------------------------------
-//	editWithFrame:inView:editor:delegate:event
-// -------------------------------------------------------------------------------
-- (void)editWithFrame:(NSRect)aRect inView:(NSView*)controlView editor:(NSText*)textObj delegate:(id)anObject event:(NSEvent*)theEvent
-{
-	NSRect textFrame = [self titleRectForBounds:aRect];
-	[super editWithFrame:textFrame inView:controlView editor:textObj delegate:anObject event:theEvent];
-}
-
-// -------------------------------------------------------------------------------
-//	selectWithFrame:inView:editor:delegate:event:start:length
-// -------------------------------------------------------------------------------
-- (void)selectWithFrame:(NSRect)aRect inView:(NSView*)controlView editor:(NSText*)textObj delegate:(id)anObject start:(NSInteger)selStart length:(NSInteger)selLength
-{
-	NSRect textFrame = [self titleRectForBounds:aRect];
-	[super selectWithFrame:textFrame inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
-}
-
-// -------------------------------------------------------------------------------
-//	drawWithFrame:cellFrame:controlView:
-// -------------------------------------------------------------------------------
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView*)controlView
-{
-	if (image != nil)
-	{
-		// the cell has an image: draw the normal item cell
-		NSSize imageSize;
-        NSRect imageFrame;
-
-        imageSize = [image size];
-        NSDivideRect(cellFrame, &imageFrame, &cellFrame, 3 + imageSize.width, NSMinXEdge);
- 
-        imageFrame.origin.x += kImageOriginXOffset;
-		imageFrame.origin.y -= kImageOriginYOffset;
-        imageFrame.size = imageSize;
-		
-        if ([controlView isFlipped])
-            imageFrame.origin.y += ceil((cellFrame.size.height + imageFrame.size.height) / 2);
-        else
-            imageFrame.origin.y += ceil((cellFrame.size.height - imageFrame.size.height) / 2);
-		[image compositeToPoint:imageFrame.origin operation:NSCompositeSourceOver];
-
-		NSRect newFrame = cellFrame;
-		newFrame.origin.x += kTextOriginXOffset;
-		newFrame.origin.y += kTextOriginYOffset;
-		newFrame.size.height -= kTextHeightAdjust;
-		[super drawWithFrame:newFrame inView:controlView];
+// We could manually implement expansionFrameWithFrame:inView: and drawWithExpansionFrame:inView: or just properly implement titleRectForBounds to get expansion tooltips to automatically work for us
+- (NSRect)titleRectForBounds:(NSRect)cellFrame {
+    NSRect result;
+    if (_image != nil) {
+        CGFloat imageWidth = [self _imageSize].width;
+        result = cellFrame;
+        result.origin.x += (3 + imageWidth);
+        result.size.width -= (3 + imageWidth);
+    } else {
+        result = [super titleRectForBounds:cellFrame];
     }
-	else
-	{
-		if ([self isGroupCell])
-		{
-			// Center the text in the cellFrame, and call super to do thew ork of actually drawing. 
-			CGFloat yOffset = floor((NSHeight(cellFrame) - [[self attributedStringValue] size].height) / 2.0);
-			cellFrame.origin.y += yOffset;
-			cellFrame.size.height -= (kTextOriginYOffset*yOffset);
-			[super drawWithFrame:cellFrame inView:controlView];
-		}
-	}
+    return result;
 }
 
-// -------------------------------------------------------------------------------
-//	cellSize:
-// -------------------------------------------------------------------------------
-- (NSSize)cellSize
-{
+- (void)editWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject event:(NSEvent *)theEvent {
+    [super editWithFrame:[self titleRectForBounds:aRect] inView:controlView editor:textObj delegate:anObject event:theEvent];
+}
+
+- (void)selectWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject start:(NSInteger)selStart length:(NSInteger)selLength {
+    [super selectWithFrame:[self titleRectForBounds:aRect] inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+    if (_image != nil) {
+        NSRect imageFrame = [self imageRectForBounds:cellFrame];
+        [_image drawInRect:imageFrame fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0 respectFlipped:YES hints:nil];
+        NSInteger newX = NSMaxX(imageFrame) + 3;
+        cellFrame.size.width = NSMaxX(cellFrame) - newX;
+        cellFrame.origin.x = newX;
+    }
+    [super drawWithFrame:cellFrame inView:controlView];
+}
+
+- (NSSize)cellSize {
     NSSize cellSize = [super cellSize];
-    cellSize.width += (image ? [image size].width : 0) + 3;
+    if (_image != nil) {
+        cellSize.width += [self _imageSize].width;
+    }
+    cellSize.width += 3;
     return cellSize;
 }
 
-// -------------------------------------------------------------------------------
-//	hitTestForEvent:
-//
-//	In 10.5, we need you to implement this method for blocking drag and drop of a given cell.
-//	So NSCell hit testing will determine if a row can be dragged or not.
-//
-//	NSTableView calls this cell method when starting a drag, if the hit cell returns
-//	NSCellHitTrackableArea, the particular row will be tracked instead of dragged.
-//
-// -------------------------------------------------------------------------------
-- (NSUInteger)hitTestForEvent:(NSEvent *)event inRect:(NSRect)cellFrame ofView:(NSView *)controlView
-{
-	NSInteger result = NSCellHitContentArea;
-	
-	/*NSOutlineView* hostingOutlineView = (NSOutlineView*)[self controlView];
-	if (hostingOutlineView)
-	{
-		NSInteger selectedRow = [hostingOutlineView selectedRow];
-		//BaseNode* node = [[hostingOutlineView itemAtRow:selectedRow] representedObject];
-
-		if (![node isDraggable])	// is the node isDraggable (i.e. non-file system based objects)
-			result = NSCellHitTrackableArea;
-	}*/
-		
-	return result;
+- (NSUInteger)hitTestForEvent:(NSEvent *)event inRect:(NSRect)cellFrame ofView:(NSView *)controlView {
+    NSPoint point = [controlView convertPoint:[event locationInWindow] fromView:nil];
+    // If we have an image, we need to see if the user clicked on the image portion.
+    if (_image != nil) {
+        // This code closely mimics drawWithFrame:inView:
+        NSSize imageSize = [self _imageSize];
+        NSRect imageFrame;
+        NSDivideRect(cellFrame, &imageFrame, &cellFrame, 3 + imageSize.width, NSMinXEdge);
+        
+        imageFrame.origin.x += 3;
+        imageFrame.size = imageSize;
+        // If the point is in the image rect, then it is a content hit
+        if (NSMouseInRect(point, imageFrame, [controlView isFlipped])) {
+            // We consider this just a content area. It is not trackable, nor it it editable text. If it was, we would or in the additional items.
+            // By returning the correct parts, we allow NSTableView to correctly begin an edit when the text portion is clicked on.
+            return NSCellHitContentArea;
+        }        
+    }
+    // At this point, the cellFrame has been modified to exclude the portion for the image. Let the superclass handle the hit testing at this point.
+    return [super hitTestForEvent:event inRect:cellFrame ofView:controlView];    
 }
+
 
 @end
 
